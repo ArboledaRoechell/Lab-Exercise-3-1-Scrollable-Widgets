f68f94f67291f90476425a43a41e2961986873bd
Jero E. Armentia <Armentia-Je@ndmu.local>
2024-02-28 16:02:52 +0800

    Scrollable widgets

--- lib/author_card.dart
+++ lib/author_card.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'fooderlich_theme.dart';
 import 'circle_image.dart';
-class AuthorCard extends StatelessWidget {
+class AuthorCard extends StatefulWidget {
  // 1
  final String authorName;
  final String title;
@@ -12,7 +12,14 @@ class AuthorCard extends StatelessWidget {
  required this.title,
  this.imageProvider,
  });
+
+  @override
+  State<AuthorCard> createState() => _AuthorCardState();
+}
+
+class _AuthorCardState extends State<AuthorCard> {
  // 2
+ bool _isFavorited = false;
  @override
  Widget build(BuildContext context) {
  // TODO: Replace return Container(...);
@@ -26,7 +33,7 @@ return Container(
  Row(
  children: [
  CircleImage(
- imageProvider: imageProvider,
+ imageProvider: widget.imageProvider,
  imageRadius: 28,
  ),
  // 2
@@ -36,11 +43,11 @@ return Container(
  crossAxisAlignment: CrossAxisAlignment.start,
  children: [
  Text(
- authorName,
+ widget.authorName,
  style: FooderlichTheme.lightTextTheme.headline2,
  ),
  Text(
- title,
+ widget.title,
  style: FooderlichTheme.lightTextTheme.headline3,
  )
  ],
@@ -49,15 +56,19 @@ return Container(
  ),
  // TODO: Add IconButton
  IconButton(
- // 4
- icon: const Icon(Icons.favorite_border),
+ // 1
+ icon: Icon(_isFavorited ? Icons.favorite : 
+Icons.favorite_border),
  iconSize: 30,
- color: Colors.grey[400],
- // 5
+ // 2
+ color: Colors.red[400],
  onPressed: () {
- const snackBar = SnackBar(content: Text('Favorite Pressed'));
- ScaffoldMessenger.of(context).showSnackBar(snackBar);
- }),
+ // 3
+ setState(() {
+ _isFavorited = !_isFavorited;
+ });
+ },
+),
  ],
  ),
 );
--- lib/home.dart
+++ lib/home.dart
@@ -1,70 +1,60 @@
 import 'package:flutter/material.dart';
-import 'card1.dart';
-import 'card2.dart';
-import 'card3.dart';
-// 1
+import 'screens/explore_screen.dart';
+import 'screens/recipes_screen.dart';
+
 class Home extends StatefulWidget {
- const Home({super.key});
- @override
- HomeState createState() => HomeState();
+  const Home({super.key});
+
+  @override
+  HomeState createState() => HomeState();
 }
+
 class HomeState extends State<Home> {
- // TODO: Add state variables and functions
- // 7
-int _selectedIndex = 0;
-// 8
-static List<Widget> pages = <Widget>[
-  // TODO: Replace with Card1
- const Card1(),
- // TODO: Replace with Card2
- const Card2(),
- // TODO: Replace with Card3
- const Card3(),
-];
-// 9
-void _onItemTapped(int index) {
- setState(() {
- _selectedIndex = index;
- });
-}
- @override
- Widget build(BuildContext context) {
- return Scaffold(
- appBar: AppBar(
- title: Text(
- 'Fooderlich',
- // 2
- style: Theme.of(context).textTheme.headline6,
- ),
- ),
- // TODO: Show selected tab
-body: pages[_selectedIndex],
- // 4
-  bottomNavigationBar: BottomNavigationBar(
- // 5
-    selectedItemColor:
-      Theme.of(context).textSelectionTheme.selectionColor,
- // TODO: Set selected tab bar
- // 10
-currentIndex: _selectedIndex,
-// 11
-onTap: _onItemTapped,
- // 6
-  items: const [
-    BottomNavigationBarItem(
-      icon: Icon(Icons.card_giftcard),
-      label: 'Card',
-      ),
-    BottomNavigationBarItem(
-      icon: Icon(Icons.card_giftcard),
-      label: 'Card2',
-      ),
-      BottomNavigationBarItem(
-        icon: Icon(Icons.card_giftcard),
-        label: 'Card3',
+  int _selectedIndex = 0;
+
+  static List<Widget> pages = <Widget>[
+    ExploreScreen(),
+    
+    RecipesScreen(),
+    Container(color: Colors.green),
+    Container(color: Colors.blue),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Fooderlich',
+          style: Theme.of(context).textTheme.headline6,
         ),
+      ),
+      body: pages[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        selectedItemColor: Theme.of(context).textSelectionTheme.selectionColor,
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.explore),
+            label: 'Explore',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.book),
+            label: 'Recipes',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.list),
+            label: 'To Buy',
+          ),
         ],
-        ),
- );
- }
+      ),
+    );
+  }
 }
